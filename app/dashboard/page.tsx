025f86d442bb59f4a59e86100caf898c771d4bce
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-03 11:22:24 +0100

    Implement Clerk middleware for route protection and add initial dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen bg-[#F8F8FF]">
+        <h1 className="text-4xl font-bold text-[#1C1C1C]">Dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
