cfe4903cbd5e4c76afb7769a8c9d4fd7a7fd6713
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-03 13:50:31 +0100

    Implement code changes to enhance functionality and improve performance

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-white/20 p-3 md:w-4/5  backdrop-blur-md sticky top-0 w-10/12 mx-auto rounded-2xl border border-black/40  mt-2 z-40 ">
-      <div className="text-xl font-semibold">WishCube</div>
+      <Link href={"/dashboard"} className="flex items-center gap-2">
+        <div className="text-xl font-semibold">WishCube</div>
+      </Link>
       <SignInButton mode="modal">
         <button className="bg-[#1C1C1C] cursor-pointer text-[#F8F8FF] py-2 px-3 rounded-xl hover:bg-[#2C2C2C] transition duration-300 ease-in-out">
           Get Started
--- app/gift/components/walletCard.tsx
+++ app/gift/components/walletCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useState } from "react";
 import {
   GoArrowDownLeft,
@@ -11,12 +12,14 @@ import { VscEye, VscEyeClosed } from "react-icons/vsc";
 const WalletCard = () => {
   const [showBalance, setShowBalance] = useState(false);
   const [showGiftingBalance, setShowGiftingBalance] = useState(false);
+
   return (
-    <div>
-      <div className="bg-white/10 bg-clip-padding backdrop-filter backdrop-blur-sm  shadow-xl p-6 rounded-2xl md:w-1/2 w-full space-y-6">
-        <h2 className="text-lg font-semibold text-gray-800">Wallet Card</h2>
+    <div className="py-6">
+      <div className="bg-[#1C1C1C] bg-[url('/doodle.webp')] bg-cover bg-center bg-blend-overlay md:w-1/2 w-11/12 mx-auto mt-3.5 rounded-2xl shadow-xl overflow-hidden backdrop-blur-sm p-6 space-y-6">
+        <h2 className="text-lg font-semibold text-gray-200">Wallet Card</h2>
+
         <div className="flex flex-col gap-4">
-          <div className="flex items-center justify-between p-4 bg-gray-100 rounded-lg">
+          <div className="flex items-center justify-between p-4 bg-gray-200 rounded-lg">
             <span className="text-sm font-medium text-gray-700">Balance</span>
             <div className="flex items-center gap-3.5">
               <span className="text-lg font-bold text-gray-900">
@@ -26,7 +29,7 @@ const WalletCard = () => {
                 onClick={() => setShowBalance(!showBalance)}
                 className="cursor-pointer"
               >
-                {showBalance ? (
+                {!showBalance ? (
                   <VscEye className="size-5" />
                 ) : (
                   <VscEyeClosed className="size-5" />
@@ -35,7 +38,7 @@ const WalletCard = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-between p-4 bg-gray-100 rounded-lg">
+          <div className="flex items-center justify-between p-4 bg-gray-200 rounded-lg">
             <span className="text-sm font-medium text-gray-700">
               Gifting Point Balance
             </span>
@@ -47,7 +50,7 @@ const WalletCard = () => {
                 onClick={() => setShowGiftingBalance(!showGiftingBalance)}
                 className="cursor-pointer"
               >
-                {showGiftingBalance ? (
+                {!showGiftingBalance ? (
                   <VscEye className="size-5" />
                 ) : (
                   <VscEyeClosed className="size-5" />
@@ -56,30 +59,34 @@ const WalletCard = () => {
             </div>
           </div>
         </div>
-        <div className=" flex items-center  gap-5 mx-auto justify-center p-4 ">
-          <div className=" bg-gray-100 rounded-lg p-4 flex flex-col items-center  w-1/5">
-            <span className=" font-medium text-gray-700 bg-gray-300 p-2 rounded-full flex items-center justify-center h-12 gap-2.5 cursor-pointer hover:bg-gray-400 transition duration-200 w-12">
-              <SlWallet className=" size-6" />
+
+        <div className="flex items-center gap-5 justify-center pt-4">
+          <div className="bg-gray-200 rounded-lg px-4 flex flex-col items-center w-1/6 py-2.5">
+            <span className="font-medium text-gray-700 bg-gray-400 p-1 rounded-full flex items-center justify-center h-10 w-10 cursor-pointer hover:bg-gray-400 transition duration-200">
+              <SlWallet className="size-4" />
             </span>
-            <span>Fund Wallet</span>
+            <span className="text-sm mt-2 whitespace-nowrap">Fund Wallet</span>
           </div>
-          <div className=" bg-gray-100 rounded-lg p-4 flex flex-col items-center  w-1/5">
-            <span className=" font-medium text-gray-700 bg-gray-300 p-2 rounded-full flex items-center justify-center h-12 gap-2.5 cursor-pointer hover:bg-gray-400 transition duration-200 w-12">
-              <GoArrowSwitch className=" size-6" />
-            </span>
-            <span>Convert</span>
+          <div className="bg-gray-200 rounded-lg px-2 flex flex-col items-center w-1/6 py-2.5">
+            <Link href="/buy">
+              <span className="font-medium text-gray-700 bg-gray-400 p-1 rounded-full flex items-center justify-center h-10 w-10 cursor-pointer hover:bg-gray-400 transition duration-200">
+                <GoArrowSwitch className="size-4" />
+              </span>
+              <span className="text-sm mt-2 whitespace-nowrap">Buy Gift</span>
+            </Link>
           </div>
-          <div className=" bg-gray-100 rounded-lg p-4 flex flex-col items-center  w-1/5">
-            <span className=" font-medium text-gray-700 bg-gray-300 p-2 rounded-full flex items-center justify-center h-12 gap-2.5 cursor-pointer hover:bg-gray-400 transition duration-200 w-12">
-              <GoArrowDownLeft className=" size-6" />
+          <div className="bg-gray-200 rounded-lg px-2 flex flex-col items-center w-1/6 py-2.5">
+            <span className="font-medium text-gray-700 bg-gray-400 p-1 rounded-full flex items-center justify-center h-10 w-10 cursor-pointer hover:bg-gray-400 transition duration-200">
+              <GoArrowDownLeft className="size-4" />
             </span>
-            <span>Receive Gift</span>
+            <span className="text-sm mt-2 whitespace-nowrap">Receive Gift</span>
           </div>
-          <div className=" bg-gray-100 rounded-lg p-4 flex flex-col items-center  w-1/5">
-            <span className=" font-medium text-gray-700 bg-gray-300 p-2 rounded-full flex items-center justify-center h-12 gap-2.5 cursor-pointer hover:bg-gray-400 transition duration-200 w-12">
-              <GoArrowDownRight className=" size-6" />
+
+          <div className="bg-gray-200 rounded-lg px-2 flex flex-col items-center w-1/6 py-2.5">
+            <span className="font-medium text-gray-700 bg-gray-400 p-1 rounded-full flex items-center justify-center h-10 w-10 cursor-pointer hover:bg-gray-400 transition duration-200">
+              <GoArrowDownRight className="size-4" />
             </span>
-            <span>Give Gift</span>
+            <span className="text-sm mt-2 whitespace-nowrap">Give Gift</span>
           </div>
         </div>
       </div>
