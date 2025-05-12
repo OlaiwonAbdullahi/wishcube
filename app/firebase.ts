a86bf1a9642d7d7fbdb92a627ba33f8bda632453
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-12 20:21:35 +0100

    feat: Conditionally initialize Firebase Analytics and improve authentication setup in firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // Add this import for authentication
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,8 +15,19 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app); // Initialize Firebase Authentication
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Conditionally initialize Firebase Analytics
+let analytics = null;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
 
 // Export what you need
 export { app, analytics, auth };
