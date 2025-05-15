af502cf23fb31854c289396eec25a300dbad7042
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-15 10:36:56 +0100

    wertyu

--- app/admin/dashboard/gift/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>GiftPage</div>;
-};
-
-export default Page;
--- app/utils/linkUtils.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LinkUtils = () => {
-  return <div>LinkUtils</div>;
-};
-
-export default LinkUtils;
